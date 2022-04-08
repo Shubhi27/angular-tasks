4ddc1d5f5f6cd878118179c9f32da3e56ed9c2bc
Shubhi Giri <65133348+Shubhi27@users.noreply.github.com>
2022-04-08 11:17:41 +0530

    first task done

---
+++ src/app/age-display/age-display.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-age-display',
+  templateUrl: './age-display.component.html',
+  styleUrls: ['./age-display.component.css']
+})
+export class AgeDisplayComponent {
+age : number =  22;
+
+}
---
+++ src/app/name-display/name-display.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-display',
+  templateUrl: './name-display.component.html',
+  styleUrls: ['./name-display.component.css']
+})
+export class NameDisplayComponent {
+firstName: string = "Shubhi";
+lastName: string = "Giri";
+}
